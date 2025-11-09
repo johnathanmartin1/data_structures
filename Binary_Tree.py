917f76895873519b6b049ee0e8461ca14dd42f88
John <johnathan.martin@googlemail.com>
2025-11-09 18:40:16 +0000

    [200~added in order, post order and pre order searched to the binaru search options and also included a function that transfroms a binary tree to a binary search tree

--- Binary_Tree.py
+++ Binary_Tree.py
@@ -14,13 +14,26 @@ class Node:
         self.data=data
         self.right=None
         self.left=None
-    
+
+'''__________________________________________________________________________
+_____________________BINARY TREE CLASS USING THE NODE CLASS__________________
+_____________________________________________________________________________'''
+
 class BinaryTree:
-    #constructor for the binary tree
+    '''Binary tree constrtuctor'''
     def __init(self):
         self.root=None
+        self.tree_type=None #assinging a tree type so that methods can be filtered 
     
-    '''recursive method called by build_breadth_tree function'''
+    
+    '''_______________________________________________________________________
+    ________________________BUILD TREE METHODS_______________________________
+    _________________________________________________________________________'''
+    
+    
+    '''********************************************************************'''
+    '''recursive method called by build_breadth_tree function to build a
+    tree filling all layers from left to right'''
     @classmethod 
     def build_breadth(cls,data,index=0):
         if index>=len(data) or data[index] is None: #checking the data exists
@@ -30,9 +43,14 @@ class BinaryTree:
         node.right = cls.build_breadth(data, 2*index+2)
         return node
     
-    '''user callable method to build a breadth first tree'''
+    '''user callable method to build a breadth first tree from a list filling
+    the layers left to right'''
     def build_breadth_tree(self,data):
+        self.tree_type='binary tree'
         self.root =  self.build_breadth(data)
+           
+    '''******************************************************************'''
+   
     
     '''recursive method to build a binary search tree called 
     by build_binary_search_tree function'''
@@ -47,19 +65,80 @@ class BinaryTree:
         node.right = cls.build_binary_search(data[root_index+1:]) 
         return node
     
-    '''user callable method to build binary search tree'''
+    '''user callable method to build binary search tree from a list
+    sorted or not'''
     def build_binary_search_tree(self,data):
+        self.tree_type='binary search tree'
         self.root = self.build_binary_search(data)    
+    '''*******************************************************************'''
     
-    '''depth first search method access to binary tree'''
+    
+    '''_______________________________________________________________________
+    __________________TREE ALTERATION METHODS_________________________________
+    __________________________________________________________________________'''
+    
+    
+    '''********************************************************************'''
+    '''inversion recuresive function called by invert method'''
     @classmethod
-    def depth_first_search(cls,node,list_nodes):
+    def invert_recursive(cls,node):
+        if node is None:
+            return
+        temp = node.right
+        node.right = cls.invert_recursive(node.left)
+        node.left = cls.invert_recursive(temp)
+        return node
+    
+    '''user callable function to invert a binary tree'''
+    def invert(self):
+        current = self.root
+        self.invert_recursive(current)
+    '''*********************************************************************'''
+    
+    '''converts a binary tree into a binary search tree'''        
+    def convert_to_binary_search(self):
+        if self.tree_type == 'binary search tree':
+            return
+        tree = sorted([element for element in self.get_tree() if element is not None])
+        self.root = self.build_binary_search(tree)
+        self.tree_type = 'binary search tree'
+        
+        
+    '''_______________________________________________________________________
+    _______________________TREE SEARCH METHODS________________________________________
+    __________________________________________________________________________'''
+    
+    
+    '''********************************************************************'''
+    '''depth first search pre order method access to binary tree'''
+    @classmethod
+    def depth_first_search_pre_order(cls,node,list_nodes):
         if node is None:
             list_nodes.append(None)
         else:
             list_nodes.append(node.data)
-            cls.depth_first_search(node.left,list_nodes)
-            cls.depth_first_search(node.right,list_nodes) 
+            cls.depth_first_search_pre_order(node.left,list_nodes)
+            cls.depth_first_search_pre_order(node.right,list_nodes) 
+    
+    '''depth first search post order method access to binary tree'''
+    @classmethod
+    def depth_first_search_post_order(cls,node,list_nodes):
+        if node is None:
+            list_nodes.append(None)
+        else:
+            cls.depth_first_search_post_order(node.left,list_nodes)
+            cls.depth_first_search_post_order(node.right,list_nodes) 
+            list_nodes.append(node.data)
+            
+    '''depth first search in order method access to binary tree'''
+    @classmethod
+    def depth_first_search_in_order(cls,node,list_nodes):
+        if node is None:
+            list_nodes.append(None)
+        else:
+            cls.depth_first_search_in_order(node.left,list_nodes)
+            list_nodes.append(node.data)
+            cls.depth_first_search_in_order(node.right,list_nodes) 
     
     '''breadth first search method to access binary tree'''
     @classmethod
@@ -80,26 +159,46 @@ class BinaryTree:
             list_nodes.append(current_level)
             
             
-    '''method to print out the binary tree with search type option'''
-    def display(self,search_type='dfs'):
+    '''method to get the binary tree with search type option in a list'''
+    def get_tree(self,search_type='dfspre'):
         nodes=[]
         current=self.root
-        if search_type=='dfs':
-            self.depth_first_search(current,nodes) 
+        if search_type == 'dfspre':
+            self.depth_first_search_pre_order(current,nodes)
+        if search_type == 'dfspost':
+            self.depth_first_search_post_order(current, nodes)
+        if search_type == 'dfsin':
+            self.depth_first_search_in_order(current, nodes)
         if search_type == 'bfs':
             self.breadth_first_search(current,nodes)
-        print(nodes)
-
+        return nodes
+    '''******************************************************************'''
+    
+'''_________________________________________________________________________
+________________END OF BINARY TREE CLASS____________________________________
+__________________________________________________________________________'''
 if __name__=='__main__':
     
     bt=BinaryTree()
   
-    practice = [1,10,15,6,3,134,9]
+    practice = [1,2,3,4,5]
     
-    bt.build_binary_search_tree(practice)
+    #bt.build_binary_search_tree(practice)
     
-    #bt.build_breadth_tree(practice)
+    bt.build_breadth_tree(practice)
+    print(bt.tree_type)
+    print(bt.get_tree('bfs'))
     
-    bt.display('bfs')
+    
+    
+    bt.convert_to_binary_search()
+  
+    
+    #bt.invert()
+    
+    
+    print(bt.tree_type)
+    print(bt.get_tree('bfs'))
+    
     
         
